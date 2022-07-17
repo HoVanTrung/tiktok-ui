7dba2e1f3f2e4dfe870ca8d058bd0db3fcdc8921
unknown <hvtrungg@gmail.com>
2022-07-18 01:00:29 +0700

    update search header

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+  logo: require('~/assets/images/logo.svg').default,
+  search: require('~/assets/images/search.svg').default,
+  close: require('~/assets/images/close.svg').default,
+};
+export default images;
